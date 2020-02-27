fb126f22708d139bdab692e0a6a721bd1e6ec67d
skcodehub <44783634+skcodehub@users.noreply.github.com>
2020-02-26 16:10:36 -0800

    Uploading stage_redshift.py
    With changes to schedule and stagetoRedshiftOperator call

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -6,6 +6,7 @@ from airflow.contrib.hooks.aws_hook import AwsHook
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
     copy_sql = """
+        TRUNCATE TABLE {};
         COPY {}
         FROM '{}'
         format as json '{}'
@@ -24,6 +25,7 @@ class StageToRedshiftOperator(BaseOperator):
                  s3_bucket="",
                  s3_key="",
                  json_path="auto",
+                 #st_date="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
@@ -36,16 +38,31 @@ class StageToRedshiftOperator(BaseOperator):
         self.s3_key = s3_key
         self.aws_credentials_id = aws_credentials_id
         self.json_path = json_path
-        
+        #self.st_date = st_date
     def execute(self, context):
-        self.log.info('Copying from S3 to Redshift')
+        self.log.info('Copying {} from S3 to Redshift'.format(self.table)) 
+        #st_date = self.st_date
+        st_date=context.get("execution_date")
+        s3_path=""
+        if self.s3_key == "log_data":
+            s3_path="s3://{}/{}/{}/{}/{}-events.json".format(self.s3_bucket, self.s3_key,st_date.year, st_date.month, st_date.strftime("%Y-%m-%d"))
+        else:
+            s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+        self.log.info('Loading {}'.format(s3_path))
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+        #s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+       
+        #if self.s3_key == "log_data":
+         #   s3_path="s3://{}/{}/{}/{}/{}-events.json".format(self.s3_bucket, self.s3_key,st_date.year, st_date.month, st_date.strftime("%Y-%m-%d"))
+        #else:
+        #    s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
         formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
+            self.table,
             s3_path,
+            #self.s3_key,
             self.json_path,
             credentials.access_key,
             credentials.secret_key
